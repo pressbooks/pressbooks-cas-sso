b879493df7be230982817b280e2e1f0ff54afdc2
Dac Chartrand <dac.chartrand@gmail.com>
2018-04-26 13:58:29 -0400

    Test boilerplate code

--- tests/test-namespace.php
+++ tests/test-namespace.php
@@ -2,6 +2,23 @@
 
 class NamespaceTest extends \WP_UnitTestCase {
 
+
+	/**
+	 * Test PB style class initializations
+	 */
+	public function test_classInitConventions() {
+		$classes = [
+			'\Pressbooks\CAS\Admin',
+			'\Pressbooks\CAS\CAS',
+			'\Pressbooks\CAS\Updates',
+		];
+		foreach ( $classes as $class ) {
+			$result = $class::init();
+			$this->assertInstanceOf( $class, $result );
+		}
+	}
+
+
 	public function test_blade() {
 		$blade = \Pressbooks\CAS\blade();
 		$this->assertTrue( is_object( $blade ) );
