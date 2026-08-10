b70c7e770432b32a9672f5c6557db594c6697a6a
Ricardo Aragon <richard015ar@gmail.com>
2026-08-10 18:21:39 +0200

    refactor: migrate to PSR-4 autoloading and laravel pint
    Replace HM Autoloader register_class_path with Composer PSR-4: move
    inc/ classes to src/ (Admin, CAS) and load the namespace.php helper
    via autoload.files. Always require vendor/autoload.php so classes
    register even when phpCAS is already loaded elsewhere.
    
    Swap pressbooks/coding-standards (PHPCS) for laravel/pint (PSR-12);
    add pint.json, repoint fix/standards scripts, drop phpcs.ruleset.xml
    and the phpcompatibility dev dep. Pin config.platform.php to 8.3 so
    the lock stays valid on the 8.3 and 8.4 CI matrices. Point phpunit
    coverage at ./src.

--- pressbooks-cas-sso.php
+++ pressbooks-cas-sso.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:         Pressbooks CAS Single Sign-On
  * Plugin URI:          https://pressbooks.org
@@ -19,36 +20,22 @@
  */
 
 // -------------------------------------------------------------------------------------------------------------------
-// Class autoloader
+// Composer autoloader (PSR-4 classes + namespace.php helpers + vendor dependencies)
 // -------------------------------------------------------------------------------------------------------------------
-\HM\Autoloader\register_class_path( 'PressbooksCasSso', __DIR__ . '/inc' );
-
-// -------------------------------------------------------------------------------------------------------------------
-// Composer autoloader
-// -------------------------------------------------------------------------------------------------------------------
-if ( ! class_exists( '\phpCAS' ) ) {
-	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-		require_once __DIR__ . '/vendor/autoload.php';
-	} else {
-		$title = __( 'Dependencies Missing', 'pressbooks-cas-sso' );
-		$body = __( 'Please run <code>composer install</code> from the root of the Pressbooks CAS Single Sign-On plugin directory.', 'pressbooks-cas-sso' );
-		$message = "<h1>{$title}</h1><p>{$body}</p>";
-		wp_die( $message, $title );
-	}
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+} else {
+    $title = __('Dependencies Missing', 'pressbooks-cas-sso');
+    $body = __('Please run <code>composer install</code> from the root of the Pressbooks CAS Single Sign-On plugin directory.', 'pressbooks-cas-sso');
+    $message = "<h1>{$title}</h1><p>{$body}</p>";
+    wp_die($message, $title);
 }
-
-// -------------------------------------------------------------------------------------------------------------------
-// Requires
-// -------------------------------------------------------------------------------------------------------------------
-
-require __DIR__ . '/inc/namespace.php';
 
 // -------------------------------------------------------------------------------------------------------------------
 // Hooks
 // -------------------------------------------------------------------------------------------------------------------
-
-add_action( 'plugins_loaded', function () {
-	\Pressbooks\Container::get( 'Blade' )->addNamespace( 'PressbooksCasSso', __DIR__ . '/templates' );
-} );
-add_action( 'plugins_loaded', [ '\PressbooksCasSso\CAS', 'init' ] );
-add_action( 'plugins_loaded', [ '\PressbooksCasSso\Admin', 'init' ] );
+add_action('plugins_loaded', function () {
+    \Pressbooks\Container::get('Blade')->addNamespace('PressbooksCasSso', __DIR__ . '/templates');
+});
+add_action('plugins_loaded', [ '\PressbooksCasSso\CAS', 'init' ]);
+add_action('plugins_loaded', [ '\PressbooksCasSso\Admin', 'init' ]);
